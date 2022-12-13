573dd8696330769989d58d8df2222083863cff85
Redux234 <lorenzo.gaetano2002@gmail.com>
2022-12-13 11:37:51 +0100

    milestone 1, mancwa la data

---
+++ GestoreEventi/Evento.cs
@@ -0,0 +1,118 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestoreEventi
+{
+    public class Evento
+    {
+        private string titolo;
+        private string data;
+        private readonly int capienza;
+        private readonly int prenotazioni;
+        public int prenotaposti;
+        public int disdiciposti;
+
+
+        public Evento(string titolo, string data, int capienza, int prenotazioni)
+        {
+
+            if (titolo == null || titolo == "")
+            {
+                throw new ArgumentOutOfRangeException("titolo", "L'evento deve avere un nome");
+            }
+            else
+            {
+                this.titolo = titolo;
+            }
+            this.data = data;
+            if (capienza <= 0)
+            {
+                throw new ArgumentOutOfRangeException("capienza", "La capienza dell'evento non può essere minore o uguale a 0");
+            }
+            else
+            {
+                this.capienza = capienza;
+            }
+            this.prenotazioni = 0 + this.prenotaposti - this.disdiciposti;
+        }
+
+        public string Gettitolo()
+        {
+            return titolo;
+        }
+        public string Getdata()
+        {
+            return data;
+        }
+        public int Getcapienza()
+        {
+            return capienza;
+        }
+        public int Getprenotazioni()
+        {
+            return prenotazioni;
+        }
+        public int Getprenotaposti()
+        {
+            return prenotaposti;
+        }
+        public int Getdisdiciposti()
+        {
+            return disdiciposti;
+        }
+
+        public void Settitolo(string titolo)
+        {
+            if(titolo == null || titolo == "")
+            {
+                throw new ArgumentOutOfRangeException("titolo", "L'evento deve avere un nome");
+            }
+            else
+            {
+                this.titolo = titolo;
+            }
+            
+        }
+        public void Setdata(string data)
+        {
+           
+            this.data = data;
+        }
+        public void Setprenotaposti(int prenotaposti)
+        {
+            if (prenotaposti > this.capienza)
+            {
+                throw new ArgumentOutOfRangeException("prenotaposti", "Non puoi prenotare più posti della capienza massima");
+            }
+            else if(prenotaposti < 0)
+            {
+                throw new ArgumentOutOfRangeException("prenotaposti", "Non puoi prenotare un numero negativo di posti");
+            }
+            else
+            {
+                this.prenotaposti = prenotaposti;
+            }
+            
+        }
+        public void Setdisdiciposti(int disdiciposti)
+        {
+            if ( disdiciposti > this.prenotazioni)
+            {
+                throw new ArgumentOutOfRangeException("disdiciposti", "Non puoi disdire più posti di quelli disponibili");
+            }
+            else if (disdiciposti < 0)
+            {
+                throw new ArgumentOutOfRangeException("disdiciposti", "Non puoi disdire un numero negativo di posti");
+            }
+            else
+            {
+                this.disdiciposti = disdiciposti;
+            }
+            
+        }
+    }
+}
--- GestoreEventi/Program.cs
+++ GestoreEventi/Program.cs
@@ -1,2 +1,22 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+DateTime dataOraAttuale = DateTime.Now;
+
+Console.WriteLine("Inserisci una data: ");
+string dataStringa = Console.ReadLine();
+
+DateTime dataInserita = DateTime.Parse(dataStringa);
+
+TimeSpan intervalloDiTempo = dataInserita - dataOraAttuale;
+
+if (dataInserita >= dataOraAttuale)
+{
+    Console.WriteLine("La data inserita è futura!");
+    Console.WriteLine("In particolare la tua data sta " + intervalloDiTempo.Days + " giorni nel futuro!");
+}
+else
+{
+    Console.WriteLine("La data inserita è passata!");
+    Console.WriteLine("In particolare la tua data sta " + intervalloDiTempo.Days + " giorni nel futuro!");
+}
